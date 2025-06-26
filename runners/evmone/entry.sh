c05a6127dead64a6743a9c2bf9b7dee578c1f979
emailtovamos <emailtovamos@gmail.com>
2025-06-26 13:50:13 +0100

    evmone

--- runners/evmone/entry.sh
+++ runners/evmone/entry.sh
@@ -5,6 +5,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $SCRIPT_DIR
 {
-  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release && cmake --build build --parallel
+  cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 && cmake --build build --parallel
 } > /dev/null
 build/runner $@
